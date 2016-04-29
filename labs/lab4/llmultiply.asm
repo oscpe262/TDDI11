f6b75b285e56f2b730a75c333957070467e92dc8
oscpe262 <oscpe262@student.liu.se>
2016-04-29 15:58:36 +0200

    foo

---
+++ labs/lab4/llmultiply.asm
@@ -0,0 +1,103 @@
+;
+;  Test cases. All numbers are in hex.
+;
+;      111122223333 *     555566667777 =          5B061D958BF0ECA7C0481B5
+;  3456FEDCAAAA1000 * EDBA00112233FF01 = 309A912AF7188C57E62072DD409A1000
+;  FFFFEEEEDDDDCCCC * BBBBAAAA99998888 = BBBB9E2692C5DDDCC28F7531048D2C60
+;  FFFFFFFFFFFFFFFF * FFFFFFFFFFFFFFFF = FFFFFFFFFFFFFFFE0000000000000001
+;  00000001FFFFFFFF * 00000001FFFFFFFF = 0000000000000003FFFFFFFC00000001
+;  FFFEFFFFFFFFFFFF * FFFF0001FFFFFFFF = FFFE0002FFFDFFFE0001FFFE00000001
+;
+
+;  void llmultiply(unsigned long long int l1,
+;                  unsigned long long int l2,
+;                  unsigned char *result);
+        
+;  a = AH * (2 << 32) + AL
+;  b = BH * (2 << 32) + BL
+;        
+;  a * b = (AH * BH            * (2 << 64)) +
+;          (AH * BL + AL * BH) * (2 << 32)  +
+;           AL * BL
+
+; For more infromation, see the lab manual!
+        
+	SECTION .data
+
+
+
+	SECTION .text
+	ALIGN	16
+	BITS	32
+
+	;; To access parameter BH on the stack you do
+	;;   MOV EAX, [EBP + 20]
+	;; Replace the zero below with the correct value to be able
+	;; to write your assembly code a little bit more readable: 
+	;;   MOV EAX, [EBP + BH_OFF]
+AL_OFF	EQU     8	; Offset from EBP to low  bits of a (AL)
+AH_OFF	EQU     12	; Offset from EBP to high bits of a (AH)
+BL_OFF	EQU     16	; Offset from EBP to low  bits of b (BL)
+BH_OFF	EQU     20	; Offset from EBP to high bits of b (BH)
+RES_OFF	EQU     24	; Offset from EBP to result array pointer
+	;;    ^^^^^ Replace 0 with correct values above
+        
+	GLOBAL llmultiply
+
+llmultiply:
+	PUSH EBP
+	MOV EBP, ESP
+
+	;; Reset
+	
+	MOV EDX, 0
+
+	;; Result ...
+	MOV EBX, [EBP + RES_OFF]
+
+	;; AL * BL
+	MOV EAX, [EBP + AL_OFF]
+	MOV EDX, [EBP + BL_OFF]
+	MUL EDX
+
+	;; Add to result,
+	;; * | * | (AL*BL)H | (AL*BL)L 
+	MOV [EBX], EAX		
+	MOV [EBX + 4], EDX	
+
+	;; AL * BH
+	MOV EAX, [EBP + AL_OFF]
+	MOV EDX, [EBP + BH_OFF]
+	MUL EDX
+	
+	;; Add to result,
+	;; * -> c | (AL*BH)H + c | (AL*BL)H+(AL*BH)L | (AL*BL)L
+	MOV [EBX + 8], EDX  	; (AL*BH)H
+	ADD [EBX + 4], EAX	; (AL*BH)L
+	ADC [EBX + 8], dword 0	; carry
+	ADC [EBX + 12], dword 0 ; carry
+	
+	;; AH * BL
+	MOV EAX, [EBP + AH_OFF]
+	MOV EDX, [EBP + BL_OFF]
+	MUL EDX
+	
+	;; Add to result,
+	;; * -> 0 -> c | (AL*BH)H+(AH*BL)H | (AL*BL)H+(AL*BH)L+(AH*BL)L | (AL*BL)L
+	MOV [EBX + 12], dword 0 ; reset high carry
+	ADD [EBX + 4], EAX	; (AH*BL)L
+	ADC [EBX + 8], EDX	; (AH*BL)H
+	ADC [EBX + 12], dword 0 ; carry
+	
+	;; AH * BH
+	MOV EAX, [EBP + AH_OFF]
+	MOV EDX, [EBP + BH_OFF]
+	MUL EDX
+
+	;; Add to result,
+	;; (AH*BH)H+c | (AL*BH)H+(AH*BL)H+(AH*BH)L | (AL*BL)H+(AL*BH)L+(AH*BL)L | (AL*BL)L
+	ADD [EBX + 8], EAX	; (AH*BH)L
+	ADC [EBX + 12], EDX	; (AH*BH)H
+	
+	POP EBP				; restore EBP reg
+	RET				;  return
---
+++ labs/lab4/main.c
@@ -0,0 +1,80 @@
+#include <libepc.h>
+
+void llmultiply(unsigned long long int l1,
+                unsigned long long int l2,
+                unsigned char *result);
+
+struct test_case {
+  unsigned long long int a;
+  unsigned long long int b;
+  unsigned long long int rh;
+  unsigned long long int rl;
+};
+
+struct test_case cases[6] = {
+
+  { 0x0000111122223333ULL, 0x0000555566667777ULL,
+    0x0000000005B061D9ULL, 0x58BF0ECA7C0481B5ULL },
+
+  { 0x3456FEDCAAAA1000ULL, 0xEDBA00112233FF01ULL,
+    0x309A912AF7188C57ULL, 0xE62072DD409A1000ULL },
+
+  { 0xFFFFEEEEDDDDCCCCULL, 0xBBBBAAAA99998888ULL,
+    0xBBBB9E2692C5DDDCULL, 0xC28F7531048D2C60ULL },
+
+  { 0xFFFFFFFFFFFFFFFFULL, 0xFFFFFFFFFFFFFFFFULL,
+    0xFFFFFFFFFFFFFFFEULL, 0x0000000000000001ULL },
+  
+  { 0x00000001FFFFFFFFULL, 0x00000001FFFFFFFFULL,
+    0x0000000000000003ULL, 0xFFFFFFFC00000001ULL },
+
+  { 0xFFFEFFFFFFFFFFFFULL, 0xFFFF0001FFFFFFFFULL,
+    0xFFFE0002FFFDFFFEULL, 0x0001FFFE00000001ULL }
+};
+
+void PutUnsignedLongLong(unsigned long long int* ulli)
+{
+  unsigned long int* uli = (unsigned long int*)ulli; 
+  PutUnsigned(uli[1], 16, 8);
+  PutUnsigned(uli[0], 16, 8); 
+}
+
+int main(int argc, char *argv[])
+{
+  unsigned char result[16];
+  int i;
+  DWORD32 startTime = 0;
+  DWORD32 stopTime = 0;
+  startTime = Milliseconds();
+  
+  ClearScreen(0x07);
+  SetCursorPosition(0, 0);
+
+  for (i = 0; i < 6; ++i)
+  {
+    PutString("Test : ");
+    PutUnsignedLongLong(&cases[i].a);
+    PutString(" * ");
+    PutUnsignedLongLong(&cases[i].b); 
+    PutString("\r\n");
+   
+    PutString("    == ");
+    PutUnsignedLongLong(&cases[i].rh);
+    PutUnsignedLongLong(&cases[i].rl);
+    PutString("\r\n");
+    
+    llmultiply(cases[i].a, cases[i].b, result);
+    
+    PutString("Result ");
+    PutUnsignedLongLong(&result[8]);
+    PutUnsignedLongLong(&result[0]); 
+    PutString("\r\n");
+    stopTime = Milliseconds() - startTime;
+
+    PutString("Time used: ");
+    PutUnsigned(stopTime,10,10);
+    
+    PutString("\r\n");
+  }
+  return 0;
+}
